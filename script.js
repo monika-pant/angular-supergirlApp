042a7c7aee4e0a13e59e703d4815bd1f72ada222
monika-pant <monikapant.1feb@gmail.com>
2018-05-04 00:21:18 +0530

    pollyfill_added_for_mediaDevices

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia ||
-navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia;
 
 var constraints = {audio: true, video: { width: 320, height: 480 } };
+if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+  }
+
+
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+  
+      // First get ahold of the legacy getUserMedia, if present
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  
+      // Some browsers just don't implement it - return a rejected promise with an error
+      // to keep a consistent interface
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+  
+      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    }
+  }
+  
 
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(localMediaStream){
     var video = document.querySelector('video');
+    // Older browsers may not have srcObject
+  if ("srcObject" in video) {
     video.srcObject = localMediaStream;
+  } else {
+    // Avoid using this in new browsers, as it is going away.
+    video.src = window.URL.createObjectURL(localMediaStream);
+  }
     video.onloadedmetadata = function(e) {
       video.play();
     };
