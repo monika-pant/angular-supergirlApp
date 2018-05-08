1f67a2d6e917cf1ef32dd4122f85a9dc431b7370
monika-pant <monikapant.1feb@gmail.com>
2018-05-08 09:37:12 +0530

    conflict esolution

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var http= require('http');
-http.createServer(function(req,res){
-res.write('Hello World');
-console.log('server started at port 8080');
-res.end();
+var express = require('express');
+var app = express();
+var path = require('path');
 
-}).listen(8080);
+// var server=app.createServer(function(req,res){
+// // console.log('server started at port 8080');
+// res.end();
+// })
+// server.listen(8080,function(err){
+//     if(err){
+//         return console.log('something bad happened', err)
+//     }
+//     console.log('server is listening on port 8080');
+    
+// });
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname + '/peerConnection.html'));
+});
+app.listen(8080);
+
